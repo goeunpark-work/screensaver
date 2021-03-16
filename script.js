afc5727b0e38f9f1fbffd87316dcee840b794bd1
Goeun Park <goeunpark.work@gmail.com>
2021-03-16 18:40:03 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function sliderChange(val) {
+    var getnumber = document.getElementById('number').value;
+    // var column_number = Math.round(getnumber * 10)
+    document.getElementById("container").style.gridTemplateColumns = 'repeat(' + getnumber+ ', 1fr)';
+
+    var font_size = 1/getnumber * 5 + 10;
+    var height = 1/getnumber * 26.4   + 33.6;
+    var paragraph = document.getElementsByClassName("caption_container");
+    var i;
+
+    for (i=0; i< paragraph.length; i++){
+        paragraph[i].style.fontSize = font_size + 'px';
+        paragraph[i].style.height = height +'px';
+    }
+  
+
+}
+
+
